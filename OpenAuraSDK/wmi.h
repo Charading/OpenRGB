61388934ebdd75082af37da2dbbae6129931a218
Stavros Avramidis <stavros9899@gmail.com>
2019-05-30 14:07:33 +0300

    Add time-out on query enumeration
    Signed-off-by: Stavros Avramidis <stavros9899@gmail.com>

--- OpenAuraSDK/wmi.h
+++ OpenAuraSDK/wmi.h
@@ -1,36 +1,61 @@
+/**
+ * @file wmi.h
+ * @author Stavros Avramidis
+ * @date 5/28/2019
+ * @brief Library bind of WMI with c++
+ */
+
 #pragma once
 
+// c++ libs
 #include <string>
 #include <vector>
 #include <map>
 #include <regex>
 
+// c libs
 #include <comdef.h>
+
+// windows libs
 #include <Wbemidl.h>
 #include <Windows.h>
 
-#pragma comment(lib, "wbemuuid.lib")
+#define WMI_WAIT_TIME 2000 /// Max time to wait for next object in when enumerating next object in wmi query in milliseconds
 
+#pragma comment(lib, "wbemuuid.lib")
 
 typedef std::map<std::string, std::string> QueryObj;
 typedef std::map<std::string, std::regex> AdditionalFilters;
 typedef std::pair<std::string, std::regex> AdditionalFilter;
 
-// Wmi Base class
-class Wmi
-{
-public:
-    Wmi();
-    ~Wmi();
+class Wmi {
+/**
+ *  Wmi Base Class
+ */
+ public:
+  Wmi();
+  ~Wmi();
+  /**
+   * @breif Initialises connection to WMI host
+   * @return HRESULT if any problem occurs
+   */
+  HRESULT init();
 
-    // Initialises connection to WMI host
-    HRESULT init();
+  /**
+   * @brief makes a wmi query
+   * @param queryStr    query sting (see https://docs.microsoft.com/en-us/windows/desktop/wmisdk/wql-sql-for-wmi)
+   * @param queryVectorOut vector of QueryObj to hold results
+   * @param filters optional pair of filters to be applied to WMi yields
+   * @return HRESULT if any problem occurs else H
+   */
+  HRESULT query(const std::string &queryStr,
+                std::vector<QueryObj> &queryVectorOut,
+                const AdditionalFilters *filters = nullptr);
 
-    HRESULT query(std::string queryStr,
-                  std::vector<QueryObj>& queryVectorOut,
-                  const AdditionalFilters* filters = nullptr);
-
-private:
-    IWbemLocator *pLoc = nullptr;
-    IWbemServices *pSvc= nullptr;
+ private:
+  IWbemLocator *pLoc = nullptr;
+  IWbemServices *pSvc = nullptr;
 };
+
+
+
