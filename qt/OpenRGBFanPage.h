0f9e52285a755432417a2af5e4c85721274f8499
Adam Honse <calcprogrammer1@gmail.com>
2020-06-06 19:41:16 -0500

    Add RPM value display for selected fan

--- qt/OpenRGBFanPage.h
+++ qt/OpenRGBFanPage.h
@@ -27,6 +27,7 @@ public:
     FanController* GetController() {return device;}
 
 private slots:
+    void UpdateRPM();
     void on_FanBox_currentIndexChanged(int index);
     void on_ModeBox_currentIndexChanged(int index);
     void on_SpeedSlider_valueChanged(int value);
