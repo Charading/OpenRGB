e736eb3800217e5c7419f9308790b1402c80092f
Felipe Urzúa <cheerpipe@gmail.com>
2020-10-30 12:47:04 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- Controllers/LogitechController/RGBController_LogitechG560.cpp
+++ Controllers/LogitechController/RGBController_LogitechG560.cpp
@@ -33,6 +33,25 @@ RGBController_LogitechG560::RGBController_LogitechG560(LogitechG560Controller* l
     Static.color_mode       = MODE_COLORS_PER_LED;
     modes.push_back(Static);
 
+    mode Cycle;
+    Cycle.name              = "Cycle";
+    Cycle.value             = LOGITECH_G560_MODE_CYCLE;
+    Cycle.flags             = MODE_FLAG_HAS_SPEED | MODE_FLAG_HAS_BRIGHTNESS;
+    Cycle.color_mode        = MODE_COLORS_PER_LED;
+    Cycle.speed_min         = LOGITECH_G560_SPEED_SLOWEST;
+    Cycle.speed_max         = LOGITECH_G560_SPEED_FASTEST;
+    Cycle.speed             = LOGITECH_G560_SPEED_NORMAL;
+    modes.push_back(Cycle);
+
+    mode Breathing;
+    Breathing.name          = "Breathing";
+    Breathing.value         = LOGITECH_G560_MODE_BREATHING;
+    Breathing.flags         = MODE_FLAG_HAS_PER_LED_COLOR | MODE_FLAG_HAS_SPEED | MODE_FLAG_HAS_BRIGHTNESS;
+    Breathing.color_mode    = MODE_COLORS_PER_LED;
+    Breathing.speed_min     = LOGITECH_G560_SPEED_SLOWEST;
+    Breathing.speed_max     = LOGITECH_G560_SPEED_FASTEST;
+    Breathing.speed         = LOGITECH_G560_SPEED_NORMAL;
+    modes.push_back(Breathing);
     SetupZones();
 }
 
