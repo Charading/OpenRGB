4272246031ca58bc870124afc17042de15efc79b
silas <herosilas12@gmail.com>
2020-12-21 13:56:04 -0600

    Still broken but I am getting there

--- PluginLoader.cpp
+++ PluginLoader.cpp
@@ -40,8 +40,10 @@ void PluginManager::ScanAndLoadPlugins()
 void PluginManager::SupplyNeedeedInfo(ORGBPluginInterface *ORGBPlugin,bool DarkTheme)
 {
     ORGBPlugin->DefineNeeded();
+    qDebug() << ORGBPlugin->NeededInfo.Needed.size();
     if (ORGBPlugin->NeededInfo.Needed.size() > 0) // Make sure that the plugin isn't independant
     {
+        qDebug() << "Getting info for plugins";
         for (int i = 0; i < int(ORGBPlugin->NeededInfo.Needed.size()); i++) // For Needed item in the Needed vector
         {
             if (ORGBPlugin->NeededInfo.Needed[i] == 1) // If the setting is == 1 (Dark theme)
@@ -50,25 +52,28 @@ void PluginManager::SupplyNeedeedInfo(ORGBPluginInterface *ORGBPlugin,bool DarkT
             }
             else if (ORGBPlugin->NeededInfo.Needed[i] == 2) // The needed setting == 2
             {
+                qDebug() << "On the search for settings";
                 if (ORGBPlugin->NeededInfo.Settings.size() > 0) // Make sure that the settings vector contains an actual setting to get
                 {
                     // In the case that they need multiple settings it should be able to get all of them
                     std::vector<std::vector<std::string>> SettingsToGet = ORGBPlugin->NeededInfo.Settings; // Make a backup of the settings so that it can just replace the variable
-                    for (int SubI = 0; i < int(SettingsToGet.size()); SubI++) // For Setting in the Settings Vector
+                    for (int SubI = 0; SubI < int(SettingsToGet.size()); SubI++) // For Setting in the Settings Vector
                     {
-                        if (SettingsToGet[i].size() > 1) // Make sure that it contains the setting name
+                        if (SettingsToGet[SubI].size() > 1) // Check First setting vector size
                         {
                             json GetSetting;
-                            GetSetting = ResourceManager::get()->GetSettingsManager()->GetSettings(GetSetting[i][0]); // Get the setting it should be for further info
-                            for (int SubSubI = 0; i < int(SettingsToGet[SubI].size()); SubSubI++) // For setting under setting name
+                            GetSetting = ResourceManager::get()->GetSettingsManager()->GetSettings(SettingsToGet[SubI][0]); // Get the setting it should be for further info
+                            for (int SubSubI = 1; SubSubI < int(SettingsToGet[SubI].size()); SubSubI++) // For setting under setting name
                             {
                                 if (GetSetting.contains(SettingsToGet[SubI][SubSubI]))
                                 {
-                                    std::string GrabSetting = GetSetting[SubI][SubSubI];
+                                    std::string GrabSetting = GetSetting[SettingsToGet[SubI][SubSubI]];
                                     ORGBPlugin->NeededInfo.Settings[SubI][SubSubI] = GetSetting[GrabSetting];
+                                    qDebug() << QString().fromStdString(GrabSetting);
                                 }
                                 else
                                 {
+                                    qDebug() << "no setting found";
                                     ORGBPlugin->NeededInfo.Settings[SubI][SubSubI] = "NoSettingFound";
                                 }
                             }
