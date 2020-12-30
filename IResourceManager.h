5ceec8e0cc3b494daea9f612b870dc468fce74be
silas <herosilas12@gmail.com>
2020-12-30 12:41:05 -0600

    Add callbacks to plugin ResourceManager interface

--- IResourceManager.h
+++ IResourceManager.h
@@ -3,11 +3,19 @@
 
 #include "ResourceManager.h"
 
+typedef void (*DeviceListChangeCallback)(void *);
+typedef void (*DetectionProgressCallback)(void *);
+typedef void (*I2CBusListChangeCallback)(void *);
+
 class IResourceManager{
 public:
     virtual std::vector<i2c_smbus_interface*> & GetI2CBusses() = 0;
 
     virtual void RegisterRGBController(RGBController *rgb_controller) = 0;
+
+    virtual void RegisterDeviceListChangeCallback(DeviceListChangeCallback new_callback, void * new_callback_arg) = 0;
+    virtual void RegisterDetectionProgressCallback(DetectionProgressCallback new_callback, void * new_callback_arg) = 0;
+    virtual void RegisterI2CBusListChangeCallback(I2CBusListChangeCallback new_callback, void * new_callback_arg) = 0;
 
     virtual std::vector<RGBController*> & GetRGBControllers() = 0;
 
@@ -17,6 +25,8 @@ public:
     virtual NetworkServer*                  GetServer() = 0;
 
     virtual SettingsManager*                GetSettingsManager() = 0;
+
+    virtual void DeviceListChanged() = 0;
 protected:
     virtual ~IResourceManager() {};
 };
