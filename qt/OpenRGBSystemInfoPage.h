591aa88ef2f3feab07103ae7577ddee9609c46ae
silas <herosilas12@gmail.com>
2021-01-26 07:16:29 -0600

    Add constant reading to SMBus tools This will allow for corsair vengeance pro reverse engineering As well as help any other devices not being read by the sniffer

--- qt/OpenRGBSystemInfoPage.h
+++ qt/OpenRGBSystemInfoPage.h
@@ -27,9 +27,22 @@ private slots:
 
     void on_ReadButton_clicked();
 
+    void on_ConstantRead_clicked();
+
+    void on_SwitchConstToCurrent_clicked();
+
 private:
     Ui::OpenRGBSystemInfoPageUi *ui;
     std::vector<i2c_smbus_interface *>& busses;
+
+    bool ConstantRead = false;
+    i2c_smbus_interface* ConstBus = nullptr;
+    uchar Constaddr   = -1;
+    uchar ConstReg    = -1;
+    uchar ConstSize   = -1;
+
+
+    void CreateReadThread();
 };
 
 #endif // OPENRGBSYSTEMINFOPAGE_H
