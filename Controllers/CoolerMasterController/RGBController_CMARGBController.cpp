7c319fd8218953e0a491a3f9df06f2fb06400776
Chris <galdarian@gmail.com>
2021-02-05 15:34:23 +1100

    Updated CM ARGB Controller to v0023 protocol
    * All RGB Modes are working
    * All ARGB Modes are working sans Direct
    * Direct Mode left available so zones are resizable

--- Controllers/CoolerMasterController/RGBController_CMARGBController.cpp
+++ Controllers/CoolerMasterController/RGBController_CMARGBController.cpp
@@ -122,6 +122,19 @@ RGBController_CMARGBController::RGBController_CMARGBController(CMARGBController 
         Rainbow.speed           = speed;
         modes.push_back(Rainbow);
 
+        mode Static;
+        Refill.name             = "Static";
+        Refill.value            = CM_ARGB_MODE_STATIC;
+        Refill.flags            = MODE_FLAG_HAS_MODE_SPECIFIC_COLOR;
+        Refill.colors_min       = 1;
+        Refill.colors_max       = 1;
+        Refill.colors.resize(Reload.colors_max);
+        Refill.speed_min        = CM_ARGB_SPEED_SLOWEST;
+        Refill.speed_max        = CM_ARGB_SPEED_FASTEST;
+        Refill.color_mode       = MODE_COLORS_MODE_SPECIFIC;
+        Refill.speed            = speed;
+        modes.push_back(Refill);
+
         mode Direct;
         Direct.name             = "Direct";
         Direct.value            = CM_ARGB_MODE_DIRECT;
@@ -132,16 +145,22 @@ RGBController_CMARGBController::RGBController_CMARGBController(CMARGBController 
         mode PassThru;
         PassThru.name           = "Pass Thru";
         PassThru.value          = CM_ARGB_MODE_PASSTHRU;
+        PassThru.flags          = 0;
         PassThru.color_mode     = MODE_COLORS_NONE;
         modes.push_back(PassThru);
     }
     else
     {
-        mode Off;
-        Off.name                = "Turn Off";
-        Off.value               = CM_RGB_MODE_OFF;
-        Off.color_mode          = MODE_COLORS_NONE;
-        modes.push_back(Off);
+        mode Static;
+        Static.name             = "Static";
+        Static.value            = CM_RGB_MODE_STATIC;
+        Static.flags            = MODE_FLAG_HAS_MODE_SPECIFIC_COLOR;
+        Static.colors_min       = 1;
+        Static.colors_max       = 1;
+        Static.colors.resize(Static.colors_max);
+        Static.color_mode       = MODE_COLORS_MODE_SPECIFIC;
+        Static.speed            = 0;
+        modes.push_back(Static);
 
         mode Breathing;
         Breathing.name          = "Breathing";
@@ -153,37 +172,46 @@ RGBController_CMARGBController::RGBController_CMARGBController(CMARGBController 
         Breathing.speed_min     = CM_ARGB_SPEED_SLOWEST;
         Breathing.speed_max     = CM_ARGB_SPEED_FASTEST;
         Breathing.color_mode    = MODE_COLORS_RANDOM;
-        Breathing.speed         = speed;
+        Breathing.speed         = CM_ARGB_SPEED_NORMAL;
         modes.push_back(Breathing);
 
         mode Flash;
-        Breathing.name          = "Flash";
-        Breathing.value         = CM_RGB_MODE_FLASH;
-        Breathing.flags         = MODE_FLAG_HAS_SPEED | MODE_FLAG_HAS_MODE_SPECIFIC_COLOR | MODE_FLAG_HAS_RANDOM_COLOR;
-        Breathing.colors_min    = 1;
-        Breathing.colors_max    = 1;
-        Breathing.colors.resize(Breathing.colors_max);
-        Breathing.speed_min     = CM_ARGB_SPEED_SLOWEST;
-        Breathing.speed_max     = CM_ARGB_SPEED_FASTEST;
-        Breathing.color_mode    = MODE_COLORS_RANDOM;
-        Breathing.speed         = speed;
-        modes.push_back(Breathing);
+        Flash.name              = "Flash";
+        Flash.value             = CM_RGB_MODE_FLASH;
+        Flash.flags             = MODE_FLAG_HAS_SPEED | MODE_FLAG_HAS_MODE_SPECIFIC_COLOR | MODE_FLAG_HAS_RANDOM_COLOR;
+        Flash.colors_min        = 1;
+        Flash.colors_max        = 1;
+        Flash.colors.resize(Flash.colors_max);
+        Flash.speed_min         = CM_ARGB_SPEED_SLOWEST;
+        Flash.speed_max         = CM_ARGB_SPEED_FASTEST;
+        Flash.color_mode        = MODE_COLORS_RANDOM;
+        Flash.speed             = CM_ARGB_SPEED_NORMAL;
+        modes.push_back(Flash);
 
-        mode ColourCycle;
-        ColourCycle.name          = "Colour Cycle";
-        ColourCycle.value         = CM_RGB_MODE_COLOUR_CYCLE;
-        ColourCycle.flags         = MODE_FLAG_HAS_SPEED;
-        ColourCycle.speed_min     = CM_ARGB_SPEED_SLOWEST;
-        ColourCycle.speed_max     = CM_ARGB_SPEED_FASTEST;
-        ColourCycle.color_mode    = MODE_COLORS_NONE;
-        ColourCycle.speed         = speed;
-        modes.push_back(ColourCycle);
+        mode Mirage;
+        Mirage.name             = "Mirage";
+        Mirage.value            = CM_RGB_MODE_MIRAGE;
+        Mirage.flags            = MODE_FLAG_HAS_SPEED | MODE_FLAG_HAS_MODE_SPECIFIC_COLOR | MODE_FLAG_HAS_RANDOM_COLOR;
+        Mirage.colors_min       = 1;
+        Mirage.colors_max       = 1;
+        Mirage.colors.resize(Mirage.colors_max);
+        Mirage.speed_min        = CM_ARGB_SPEED_SLOWEST;
+        Mirage.speed_max        = CM_ARGB_SPEED_FASTEST;
+        Mirage.color_mode       = MODE_COLORS_RANDOM;
+        Mirage.speed            = CM_ARGB_SPEED_NORMAL;
+        modes.push_back(Mirage);
 
         mode PassThru;
         PassThru.name           = "Pass Thru";
         PassThru.value          = CM_RGB_MODE_PASSTHRU;
         PassThru.color_mode     = MODE_COLORS_NONE;
         modes.push_back(PassThru);
+
+        mode Off;
+        Off.name                = "Turn Off";
+        Off.value               = CM_RGB_MODE_OFF;
+        Off.color_mode          = MODE_COLORS_NONE;
+        modes.push_back(Off);
     }
 
     Init_Controller();         //Only processed on first run
@@ -227,6 +255,11 @@ void RGBController_CMARGBController::SetupZones()
     {
         bool boolSingleLED = (zones[zone_idx].type == ZONE_TYPE_SINGLE);    //Calculated for later use
 
+        if (!boolSingleLED)
+        {
+            cmargb->SetLedCount( std::stoi(zones[zone_idx].name), zones[zone_idx].leds_count);
+        }
+
         for(unsigned int lp_idx = 0; lp_idx < zones[zone_idx].leds_count; lp_idx++)
         {
             led     new_led;
@@ -258,12 +291,18 @@ void RGBController_CMARGBController::ResizeZone(int zone, int new_size)
         return;
     }
 
-    if(((unsigned int)new_size >= zones[zone].leds_min) && ((unsigned int)new_size <= zones[zone].leds_max))
-    {
-        zones[zone].leds_count = new_size;
+    uint8_t start_zone  = ( zone == 0xFF ) ? 1 : zone;
+    uint8_t end_zone    = 1 + (( zone == 0xFF ) ? 4 : zone);
 
-        SetupZones();
+    for(std::size_t zone_idx = start_zone; zone_idx < end_zone; zone_idx++)
+    {
+        if(((unsigned int)new_size >= zones[zone_idx].leds_min) && ((unsigned int)new_size <= zones[zone_idx].leds_max))
+        {
+            zones[zone_idx].leds_count = new_size;
+        }
     }
+
+    SetupZones();
 }
 
 void RGBController_CMARGBController::DeviceUpdateLEDs()
@@ -274,8 +313,8 @@ void RGBController_CMARGBController::DeviceUpdateLEDs()
     cmargb->SetColor(red, grn, blu);*/
 
     //this one
-    cmargb->SetMode( modes[active_mode].value, modes[active_mode].speed );
-    cmargb->SetLedsDirect( zones[0].colors, zones[0].leds_count );
+    //cmargb->SetMode( modes[active_mode].value, modes[active_mode].speed );
+    //cmargb->SetLedsDirect( zones[0].colors, zones[0].leds_count );
 }
 
 void RGBController_CMARGBController::UpdateZoneLEDs(int zone)
@@ -284,7 +323,7 @@ void RGBController_CMARGBController::UpdateZoneLEDs(int zone)
     unsigned char red       = RGBGetRValue(colour);
     unsigned char grn       = RGBGetGValue(colour);
     unsigned char blu       = RGBGetBValue(colour);
-    cmargb->SetColor(red, grn, blu);
+    //cmargb->SetColor(red, grn, blu);
 }
 
 void RGBController_CMARGBController::UpdateSingleLED(int led)
@@ -300,16 +339,8 @@ void RGBController_CMARGBController::SetCustomMode()
 
 void RGBController_CMARGBController::DeviceUpdateMode()
 {
-    if( modes[active_mode].color_mode == MODE_COLORS_MODE_SPECIFIC )
-    {
-        unsigned char red = RGBGetRValue(modes[active_mode].colors[0]);
-        unsigned char grn = RGBGetGValue(modes[active_mode].colors[0]);
-        unsigned char blu = RGBGetBValue(modes[active_mode].colors[0]);
-        cmargb->SetColor(red, grn, blu);
-    }
-    else
-    {
-        cmargb->SetColor(0, 0, 0);        //If the mode is not colour specific then set colour to black for the random index
-    }
-    cmargb->SetMode( modes[active_mode].value, modes[active_mode].speed );
+    bool random_colours     = (modes[active_mode].color_mode == MODE_COLORS_RANDOM);
+    RGBColor colour         = (modes[active_mode].color_mode == MODE_COLORS_MODE_SPECIFIC) ? modes[active_mode].colors[0] : 0;
+
+    cmargb->SetMode( modes[active_mode].value, modes[active_mode].speed, colour, random_colours );
 }
