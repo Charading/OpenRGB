45516d6bf4d7b62a46aefd629afc915746796b57
Chris <galdarian@gmail.com>
2021-02-10 16:40:39 +1100

    Added brightness to profile loading and saving
    * Bumped profile version to 2
    * Changed the _string[] and _version to consistent naming across the
    manager so they can be found in future
    * Allowed for backwards compatibility with v1 profiles
    * Loading a v1 profile onto a device with brightness will work
    * Loading a v2 profile onto a device without brightness also works

--- ProfileManager.cpp
+++ ProfileManager.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <cstring>
 
+#define OPENRGB_PROFILE_VERSION 2
+
 namespace fs = std::experimental::filesystem;
 
 ProfileManager::ProfileManager(std::string config_dir)
@@ -42,7 +44,7 @@ bool ProfileManager::SaveProfile(std::string profile_name)
         | 16 bytes - "OPENRGB_PROFILE"                              |
         | 4 bytes - Version, unsigned int                           |
         \*---------------------------------------------------------*/
-        unsigned int profile_version = 1;
+        unsigned int profile_version = OPENRGB_PROFILE_VERSION;
         controller_file.write("OPENRGB_PROFILE", 16);
         controller_file.write((char *)&profile_version, sizeof(unsigned int));
 
@@ -113,18 +115,18 @@ std::vector<RGBController*> ProfileManager::LoadProfileToList
     /*---------------------------------------------------------*\
     | Read and verify file header                               |
     \*---------------------------------------------------------*/
-    char            header_string[16]{};
-    unsigned int    header_version;
+    char            profile_string[16];     //Changed for naming consistency
+    unsigned int    profile_version;
 
-    controller_file.read(header_string, 16);
-    controller_file.read((char *)&header_version, sizeof(unsigned int));
+    controller_file.read(profile_string, 16);
+    controller_file.read((char *)&profile_version, sizeof(unsigned int));
 
     controller_offset += 16 + sizeof(unsigned int);
     controller_file.seekg(controller_offset);
 
-    if(strcmp(header_string, "OPENRGB_PROFILE") == 0)
+    if(strcmp(profile_string, "OPENRGB_PROFILE") == 0)
     {
-        if(header_version == 1)
+        if(profile_version <= OPENRGB_PROFILE_VERSION)
         {
             /*---------------------------------------------------------*\
             | Read controller data from file until EOF                  |
@@ -232,17 +234,20 @@ bool ProfileManager::LoadDeviceFromListWithOptions
                 {
                     for(std::size_t mode_index = 0; mode_index < temp_controller->modes.size(); mode_index++)
                     {
-                        if((temp_controller->modes[mode_index].name       == load_controller->modes[mode_index].name      )
-                         &&(temp_controller->modes[mode_index].value      == load_controller->modes[mode_index].value     )
-                         &&(temp_controller->modes[mode_index].flags      == load_controller->modes[mode_index].flags     )
-                         &&(temp_controller->modes[mode_index].speed_min  == load_controller->modes[mode_index].speed_min )
-                         &&(temp_controller->modes[mode_index].speed_max  == load_controller->modes[mode_index].speed_max )
-                         &&(temp_controller->modes[mode_index].colors_min == load_controller->modes[mode_index].colors_min)
-                         &&(temp_controller->modes[mode_index].colors_max == load_controller->modes[mode_index].colors_max))
+                        if((temp_controller->modes[mode_index].name             == load_controller->modes[mode_index].name          )
+                         &&(temp_controller->modes[mode_index].value            == load_controller->modes[mode_index].value         )
+                         &&(temp_controller->modes[mode_index].flags            == load_controller->modes[mode_index].flags         )
+                         &&(temp_controller->modes[mode_index].speed_min        == load_controller->modes[mode_index].speed_min     )
+                         &&(temp_controller->modes[mode_index].speed_max        == load_controller->modes[mode_index].speed_max     )
+                         /*&&(temp_controller->modes[mode_index].brightness_min   == load_controller->modes[mode_index].brightness_min)   //Without knowing the version not checking the min / max values will allow controllers
+                         &&(temp_controller->modes[mode_index].brightness_max   == load_controller->modes[mode_index].brightness_max)*/   //to load existing profiles and update. This can then be turned on in a future release
+                         &&(temp_controller->modes[mode_index].colors_min       == load_controller->modes[mode_index].colors_min    )
+                         &&(temp_controller->modes[mode_index].colors_max       == load_controller->modes[mode_index].colors_max   ))
                         {
-                            load_controller->modes[mode_index].speed      = temp_controller->modes[mode_index].speed;
-                            load_controller->modes[mode_index].direction  = temp_controller->modes[mode_index].direction;
-                            load_controller->modes[mode_index].color_mode = temp_controller->modes[mode_index].color_mode;
+                            load_controller->modes[mode_index].speed            = temp_controller->modes[mode_index].speed;
+                            load_controller->modes[mode_index].brightness       = temp_controller->modes[mode_index].brightness;
+                            load_controller->modes[mode_index].direction        = temp_controller->modes[mode_index].direction;
+                            load_controller->modes[mode_index].color_mode       = temp_controller->modes[mode_index].color_mode;
 
                             load_controller->modes[mode_index].colors.resize(temp_controller->modes[mode_index].colors.size());
 
@@ -360,15 +365,15 @@ void ProfileManager::UpdateProfileList()
             /*---------------------------------------------------------*\
             | Read and verify file header                               |
             \*---------------------------------------------------------*/
-            char            header_string[16];
-            unsigned int    header_version;
+            char            profile_string[16];     //Changed for naming consistency
+            unsigned int    profile_version;
 
-            profile_file.read(header_string, 16);
-            profile_file.read((char *)&header_version, sizeof(unsigned int));
+            profile_file.read(profile_string, 16);
+            profile_file.read((char *)&profile_version, sizeof(unsigned int));
 
-            if(strcmp(header_string, "OPENRGB_PROFILE") == 0)
+            if(strcmp(profile_string, "OPENRGB_PROFILE") == 0)
             {
-                if(header_version == 1)
+                if(profile_version <= OPENRGB_PROFILE_VERSION)
                 {
                     /*---------------------------------------------------------*\
                     | Add this profile to the list                              |
