54cff805ab4a177c6bf3d8e68f607e570414f856
Chris <galdarian@gmail.com>
2021-02-22 13:47:12 +1100

    Final code polish prior to merge
    * Remove unneeded comments
    * Remove depracated functions

--- Controllers/ThermaltakeRiingController/ThermaltakeRiingQuadController.cpp
+++ Controllers/ThermaltakeRiingController/ThermaltakeRiingQuadController.cpp
@@ -38,6 +38,13 @@ ThermaltakeRiingQuadController::ThermaltakeRiingQuadController(hid_device* dev_h
     | to send a packet every TT_QUAD_KEEPALIVE seconds      |
     \*-----------------------------------------------------*/
     memset(tt_quad_buffer, 0x00, sizeof(tt_quad_buffer));       //zero out entire buffer
+    unsigned char temp_buffer[3]    = { 0x00, 0x32, 0x52 };
+    for(std::size_t zone_index = 0; zone_index < TT_QUAD_ZONES; zone_index++)
+    {
+        //Add the constant bytes for the mode info buffer
+        memcpy(&tt_quad_buffer[zone_index][0], temp_buffer, 3);
+    }
+
     keepalive_thread_run = 1;
     keepalive_thread = new std::thread(&ThermaltakeRiingQuadController::KeepaliveThread, this);
 }
@@ -86,25 +93,18 @@ void ThermaltakeRiingQuadController::SetChannelLEDs(uint8_t zone, RGBColor * col
     //would allow for higher FPS if the struct could be optimised for speed
     for(std::size_t color = 0; color < num_colors; color++)
     {
-        //int color_idx = color * 3;
-        //color_data[color_idx + 0] = RGBGetGValue(colors[color]);    //Green First
-        //color_data[color_idx + 1] = RGBGetRValue(colors[color]);
-        //color_data[color_idx + 2] = RGBGetBValue(colors[color]);
-        color_data[color * 3]       = ToGRBRaw(colors[color]);
+        int color_idx = color * 3;
+        color_data[color_idx]       = RGBGetGValue(colors[color]);    //Green First
+        color_data[color_idx + 1]   = RGBGetRValue(colors[color]);
+        color_data[color_idx + 2]   = RGBGetBValue(colors[color]);
     }
 
-<<<<<<< HEAD
-    PrepareBuffer(zone, num_colors, color_data);
-
-    //delete[] color_data;
-=======
     tt_quad_buffer[zone][TT_QUAD_ZONE_BYTE]     = zone + 1;
     tt_quad_buffer[zone][TT_QUAD_MODE_BYTE]     = current_mode + ( current_speed & 0x03 );
     memcpy(&tt_quad_buffer[zone][TT_QUAD_DATA_BYTE], color_data, (num_colors * 3));
     SendBuffer();
 
     delete[] color_data;
->>>>>>> Final code polish prior to merge
 }
 
 void ThermaltakeRiingQuadController::SetMode(unsigned char mode, unsigned char speed)
@@ -122,36 +122,11 @@ void ThermaltakeRiingQuadController::SendInit()
     hid_read_timeout(dev, buffer, buffer_size, TT_QUAD_INTERRUPT_TIMEOUT);
 }
 
-<<<<<<< HEAD
-void ThermaltakeRiingQuadController::PrepareBuffer(unsigned char zone, unsigned char num_colors, unsigned char* color_data)
-{
-    unsigned char buffer[TT_QUAD_PACKET_SIZE]   = { 0x00, 0x32, 0x52};  // 0x33, 0x51, Zone ??
-
-    buffer[TT_QUAD_ZONE_BYTE]                   = zone + 1;
-    buffer[TT_QUAD_MODE_BYTE]                   = current_mode + ( current_speed & 0x03 );
-    //memcpy(&buffer[TT_QUAD_DATA_BYTE], color_data, (num_colors * 3));   //Copy in GRB color data
-
-    //Copy the prepared buffer to the module buffer then send
-    //memcpy(&tt_quad_buffer[zone][0], buffer, TT_QUAD_PACKET_SIZE);
-    memcpy(&tt_quad_buffer[zone][TT_QUAD_DATA_BYTE], color_data, (num_colors * 3));
-    SendBuffer();
-}
-
-void ThermaltakeRiingQuadController::SendBuffer()
-{
-    unsigned char buffer[TT_QUAD_PACKET_SIZE]   = { 0x00 };     //Temporary read buffer
-
-    for(std::size_t zone_index = 0; zone_index < TT_QUAD_ZONES; zone_index++)
-    {
-        hid_write(dev, tt_quad_buffer[zone_index], TT_QUAD_PACKET_SIZE);
-        hid_read_timeout(dev, buffer, TT_QUAD_PACKET_SIZE, TT_QUAD_INTERRUPT_TIMEOUT);
-=======
 void ThermaltakeRiingQuadController::SendBuffer()
 {
     for(std::size_t zone_index = 0; zone_index < TT_QUAD_ZONES; zone_index++)
     {
         hid_write(dev, tt_quad_buffer[zone_index], TT_QUAD_PACKET_SIZE);
->>>>>>> Final code polish prior to merge
     }
     //Update the last commit time
     last_commit_time = std::chrono::steady_clock::now();
