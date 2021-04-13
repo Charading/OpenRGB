bb7c2a8cb2da2d85a8b2271a767c240865ebe921
Simbaclaws <hylke.hellinga@gmail.com>
2021-04-13 23:42:47 +0200

    Added RX580 Strix Gaming OC

--- Controllers/AsusAuraGPUController/AsusAuraGPUControllerDetect.cpp
+++ Controllers/AsusAuraGPUController/AsusAuraGPUControllerDetect.cpp
@@ -35,7 +35,8 @@ static const gpu_pci_device device_list[] =
 {
     { NVIDIA_VEN,       NVIDIA_GTX1060_DEV,     ASUS_SUB_VEN,       ASUS_GTX1060_STRIX,             0x29,     "ASUS GTX 1060 Strix"           },
     { AMD_GPU_VEN,      AMD_VEGA10_DEV,         ASUS_SUB_VEN,       ASUS_VEGA64_STRIX,              0x29,     "ASUS Vega 64 Strix"            },
-    { AMD_GPU_VEN,      AMD_NAVI10_DEV,         ASUS_SUB_VEN,       ASUS_RX5700XT_STRIX_GAMING_OC,  0x2A,     "ASUS RX 5700XT Strix Gaming OC"}
+    { AMD_GPU_VEN,      AMD_NAVI10_DEV,         ASUS_SUB_VEN,       ASUS_RX5700XT_STRIX_GAMING_OC,  0x2A,     "ASUS RX 5700XT Strix Gaming OC"},
+    { AMD_GPU_VEN,      AMD_POLARIS_DEV,        ASUS_SUB_VEN,       ASUS_RX580_STRIX_GAMING_OC,     0x29,     "ASUS RX 580 Strix Gaming OC"   }
 };
 
 /******************************************************************************************\
