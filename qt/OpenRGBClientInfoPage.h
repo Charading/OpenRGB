efb5f6c4593e6d0bced3e4c156ae37ef75a3add8
Scott Wilde <sw8lde@gmail.com>
2021-05-15 14:16:09 -0500

    Add fan controllers to SDK client view

--- qt/OpenRGBClientInfoPage.h
+++ qt/OpenRGBClientInfoPage.h
@@ -1,10 +1,12 @@
 #ifndef OPENRGBCLIENTINFOPAGE_H
 #define OPENRGBCLIENTINFOPAGE_H
 
-#include <QFrame>
 #include "RGBController.h"
 #include "ui_OpenRGBClientInfoPage.h"
 #include "NetworkClient.h"
+
+#include <QFrame>
+#include <thread>
 
 namespace Ui {
 class OpenRGBClientInfoPage;
@@ -20,6 +22,8 @@ public:
 
     void AddClient(NetworkClient* new_client);
 
+    void FanUpdateThreadFunction();
+
 public slots:
     void UpdateInfo();
 
@@ -28,7 +32,12 @@ private slots:
     void onClientDisconnectButton_clicked(QObject * arg);
 
 private:
+    void UpdateFanReadings();
+
     Ui::OpenRGBClientInfoPageUi *ui;
+
+    bool fan_thread_running;
+    std::thread* FanUpdateThread;
 
 signals:
     void ClientListUpdated();
