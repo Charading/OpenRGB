7e0b0ed76ab36f658ce658afc125c261316e3d0f
DrZlo13 <who.just.the.doctor@gmail.com>
2021-06-06 09:48:13 +1000

    Cleaned his send code

--- Controllers/AnnePro2Controller/AnnePro2Controller.cpp
+++ Controllers/AnnePro2Controller/AnnePro2Controller.cpp
@@ -39,10 +39,17 @@ void AnnePro2Controller::SendDirect(unsigned char frame_count, unsigned char * f
 {
     // Reverse engineered by https://github.com/manualmanul/Annemone
 
-    const unsigned char real_command_info_length = 3 + 1;
+    const unsigned char hid_cmd_service_data_length = 4;
+    const unsigned char hid_cmd_service_data[hid_cmd_service_data_length] = {0, 123, 16, 65};
+    const unsigned char hid_cmd_static_message_length = 3;
+    const unsigned char hid_cmd_static_message[hid_cmd_static_message_length] = {0, 0, 125};
+    const unsigned char hid_cmd_command_info_length = 3;
+    const unsigned char hid_cmd_command_info[hid_cmd_command_info_length] = {32, 3, 255};
+
+    const unsigned char real_command_info_length = hid_cmd_command_info_length + 1;
     const unsigned char max_hid_length = 64;
 
-    const unsigned char max_command_length = max_hid_length - 4 - 2 - 3;
+    const unsigned char max_command_length = max_hid_length - hid_cmd_service_data_length - 2 - hid_cmd_static_message_length;
     const unsigned char max_message_length = max_command_length - real_command_info_length;
     const unsigned char messages_to_send_amount = frame_count / max_message_length + 1;
     const unsigned char val_1 = frame_count % max_message_length;
@@ -56,24 +63,24 @@ void AnnePro2Controller::SendDirect(unsigned char frame_count, unsigned char * f
         const unsigned char a = ((messages_to_send_amount - 1) == p) ? val_2 + real_command_info_length : max_message_length + real_command_info_length;
         
         // service_data
-        hid_command[0] = 0;
-        hid_command[1] = 123;
-        hid_command[2] = 16;
-        hid_command[3] = 65;
+        hid_command[0] = hid_cmd_service_data[0];
+        hid_command[1] = hid_cmd_service_data[1];
+        hid_command[2] = hid_cmd_service_data[2];
+        hid_command[3] = hid_cmd_service_data[3];
 
         // 
         hid_command[4] = e;
         hid_command[5] = a;
 
         // static_message
-        hid_command[6] = 0;
-        hid_command[7] = 0;
-        hid_command[8] = 125;
+        hid_command[6] = hid_cmd_static_message[0];
+        hid_command[7] = hid_cmd_static_message[1];
+        hid_command[8] = hid_cmd_static_message[2];
 
         // command_info
-        hid_command[9] = 32;
-        hid_command[10] = 3;
-        hid_command[11] = 255;
+        hid_command[9] = hid_cmd_command_info[0];
+        hid_command[10] = hid_cmd_command_info[1];
+        hid_command[11] = hid_cmd_command_info[2];
         //
         hid_command[12] = 2;
 
