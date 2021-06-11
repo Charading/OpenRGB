8396228f2f6d5480b8abc3bec2bfaf018be5aa18
Alex <ritt.alex@gmail.com>
2021-06-11 20:05:23 +0200

    Add PID when the device is not connected with usb cabel

--- Controllers/SteelSeriesController/SteelSeriesControllerDetect.cpp
+++ Controllers/SteelSeriesController/SteelSeriesControllerDetect.cpp
@@ -39,6 +39,7 @@
 #define STEELSERIES_RIVAL_310_CSGO_HOWL_PID         0x171e
 #define STEELSERIES_RIVAL_310_PUBG_PID              0x1736
 #define STEELSERIES_RIVAL_650_PID                   0x172B
+#define STEELSERIES_RIVAL_650_WIRELESS_PID          0x1726
 #define STEELSERIES_SENSEI_TEN_PID                  0x1832
 #define STEELSERIES_SENSEI_TEN_CSGO_NEON_RIDER_PID  0x1834
 #define STEELSERIES_SENSEI_310_PID                  0x1722
@@ -200,6 +201,7 @@ REGISTER_HID_DETECTOR_I("SteelSeries Rival 310",                            Dete
 REGISTER_HID_DETECTOR_I("SteelSeries Rival 310 CS:GO Howl Edition",         DetectSteelSeriesSensei,    STEELSERIES_VID, STEELSERIES_RIVAL_310_CSGO_HOWL_PID,       0  );
 REGISTER_HID_DETECTOR_I("SteelSeries Rival 310 PUBG Edition",               DetectSteelSeriesSensei,    STEELSERIES_VID, STEELSERIES_RIVAL_310_PUBG_PID,            0  );
 REGISTER_HID_DETECTOR_I("SteelSeries Rival 650",                            DetectSteelSeriesRival650,  STEELSERIES_VID, STEELSERIES_RIVAL_650_PID,                 0  );
+REGISTER_HID_DETECTOR_I("SteelSeries Rival 650 Wireless",                   DetectSteelSeriesRival650,  STEELSERIES_VID, STEELSERIES_RIVAL_650_WIRELESS_PID,        0  );
 REGISTER_HID_DETECTOR_I("SteelSeries Sensei TEN",                           DetectSteelSeriesSensei,    STEELSERIES_VID, STEELSERIES_SENSEI_TEN_PID,                0  );
 REGISTER_HID_DETECTOR_I("SteelSeries Sensei TEN CS:GO Neon Rider Edition",  DetectSteelSeriesSensei,    STEELSERIES_VID, STEELSERIES_SENSEI_TEN_CSGO_NEON_RIDER_PID,0  );
 REGISTER_HID_DETECTOR_I("SteelSeries Sensei 310",                           DetectSteelSeriesSensei,    STEELSERIES_VID, STEELSERIES_SENSEI_310_PID,                0  );
