2f0c25e070784695134f18fab05cd80c7be957e7
SU wuyi <liu2017985126@qq.com>
2023-08-30 10:36:45 +0000

    use opensourece lib openJGINYUErgbV2.dll

--- Controllers/JginYueGPIOController/JginYueGPIODrive.cpp
+++ Controllers/JginYueGPIOController/JginYueGPIODrive.cpp
@@ -4,16 +4,16 @@
 
 int GPIOdrive_API(unsigned int* GRB_ptr ,unsigned int num_LED)
 {
-    typedef void (* ProcGPIOdrive)(unsigned int* GRB_ptr ,unsigned int num_LED);
+    typedef void (* ProcGPIOdrive)(unsigned int* GRB_ptr ,unsigned int num_LED,unsigned int MotherBoardtype);
     ProcGPIOdrive GPIOdrive = NULL;
     HMODULE hModule = NULL;
-    hModule = LoadLibraryA("openJGINYUErgb.dll");
+    hModule = LoadLibraryA("openJGINYUErgbV2.dll");
     if(hModule == NULL)
     {
         return 0;
     }
     GPIOdrive = (ProcGPIOdrive)GetProcAddress(hModule, "GPIOdrive");
-    GPIOdrive(GRB_ptr,num_LED);
+    GPIOdrive(GRB_ptr,num_LED,1);
     FreeLibrary(hModule);
     return 1;
 }
